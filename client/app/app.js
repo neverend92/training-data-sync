61418e17a7cbcf2fae5376ad088438519135b572
Stefan Hettich <stefan-hettich@live.de>
2014-03-21 16:15:13 +0100

    moved settings/config to json-file

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,17 @@
+// Config-Datei laden.
+var settings;
+$.ajax({
+	url: 'app/config/config.json',
+	async: false,
+	dataType: 'json',
+	success: function (data) {
+		settings = data;
+	}
+});
+console.log(settings);
+alert(settings.server.ip);
+
+// Initialisiere Ember Application
 App = Ember.Application.create();
 
 App.Router.map(function () {
