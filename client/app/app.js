df62058548d71fe1c4656aba675220f351466144
Stefan Hettich <stefan-hettich@live.de>
2014-03-24 00:00:31 +0100

    some commenting stuff

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,27 @@
-// Config-Datei laden.
+/**
+ * BEGIN Config-Datei laden.
+ */
+// Settings-Objekt, das bei erfolgreichem Laden befühlt wird.
 var settings;
 $.ajax({
+	// Ort der Config-Datei.
 	url: 'app/config/config.json',
+	// Kein asynchroner Ajax-Call, damit Settings-Objekt verfügbar ist
+	// wenn Anwendung gestartet wird.
 	async: false,
+	// Daten-Typ der Config-Datei.
 	dataType: 'json',
+	// Aktion bei erfolgreichem Laden.
 	success: function (data) {
+		// Setzen der globalen settings-Variable mit empfangenen Daten.
 		settings = data;
 	}
 });
-console.log(settings);
-alert(settings.server.ip);
+// DEBUG-Log auf Konsole.
+console.log('DEBUG: Log <settings>-Object: ' , settings);
+/**
+ * END Config-Datei laden.
+ */
 
 // Initialisiere Ember Application
 App = Ember.Application.create();
