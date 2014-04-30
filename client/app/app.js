d92022c45368f75f9d8b796ac32ef018997b1ac5
Stefan Hettich <stefan-hettich@live.de>
2014-04-30 21:51:00 +0200

    installed all components

--- client/app/app.js
+++ client/app/app.js
@@ -24,15 +24,90 @@ console.log('DEBUG: Log <settings>-Object: ' , settings);
  */
 
 // Initialisiere Ember Application
-App = Ember.Application.create();
+App = Ember.Application.create({
+	//LOG_TRANSITIONS: true
+});
 
+/**
+ * BEGIN Verbindung zum Socket
+ */
+// Array mit HTML-Code für den jeweiligen Verbindungs-Status
+var socketStatus = {
+	'online': '<span style="color: #5cb85c;"><i class="glyphicon glyphicon-ok-circle"></i> Online</span>',
+	'offline': '<span style="color: #d9534f;"><i class="glyphicon glyphicon-remove-circle"></i> Offline</span>',
+	'connect': '<span style="color: #f0ad4e;"><i class="glyphicon glyphicon-refresh"></i> Verbinde...</span>'
+};
+
+// IP des Servers aus Einstellungen
+var socketIP = settings.server.ip;
+
+// Port des Servers aus Einstellung
+var socketPort = settings.server.port;
+
+// Prüfen der abgerufenen Einstellungen
+// IP und Port dürfen nicht leer sein.
+if (socketIP == undefined || socketPort == undefined) {
+	/**
+	 * @TODO: Fehlermeldung durch Fallback ersetzen.
+	 */
+	alert('Error Loading Settings');
+	console.log('Error Loading Settings');
+}
+
+// Verbindung zum Websocket aufbauen
+// socket-Objekt erhalten.
+var socket = io.connect('http://' + socketIP + ':' + socketPort);
+
+// Binding für den Verbindungsaufbau zum Socket
+socket.on('connecting', function () {
+	$('#socketStatus').html(socketStatus.connect);
+	console.log('Verbindung zum Server wird aufgebaut...');
+});
+
+// Binding für die abgeschlossene Verbindungsherstellung
+socket.on('connect', function () {
+	$('#socketStatus').html(socketStatus.online);
+	console.log('Verbindung zum Server hergestellt.');
+	
+	socket.emit('sync-down', {
+		msg: 'Sync-Down-Request'
+	});
+});
+
+// Binding für die Verbindungstrennung
+socket.on('disconnect', function () {
+	$('#socketStatus').html(socketStatus.offline);
+	console.log('Verbindung zum Server getrennt.');
+});
+
+socket.on('sync-down', function (data) {
+	console.log(data);
+});
+socket.on('sync-down-single', function (data) {
+	console.log(data);
+});
+/**
+ * END Verbindung zum Socket
+ */
+
+/**
+ * BEGIN Ember Router
+ */
 App.Router.map(function () {
 	this.resource('smallDatas');
 });
+/**
+ * END Ember Router
+ */
 
-
+/**
+ * BEGIN Ember Route
+ */
 App.SmallDatasRoute = Ember.Route.extend({
 	model: function () {
 		
 	}
 });
+/**
+ * END Ember Route
+ */
