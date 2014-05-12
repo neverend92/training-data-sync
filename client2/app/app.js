81dcaee50b2fbe3283c22b851ef30aebf69e93a8
neverend92 <stefan-hettich@live.de>
2014-05-12 11:04:22 +0200

    improvement client 2

--- client2/app/app.js
+++ client2/app/app.js
@@ -26,6 +26,8 @@ console.log('DEBUG: Log <settings>-Object: ' , settings);
 /**
  * BEGIN Status der App laden.
  */
+$('#mainHeader').hide();
+$('#mainContent').hide();
 /*var lastSync = localStorage.get('last-sync');
 if (lastSync == undefined) {
 	localStorage.set('last-sync', null);
@@ -37,30 +39,67 @@ if (lastSync == undefined) {
 /**
  * BEGIN Datenbank-Verbindung
  */
+// Objekt, das die Verbindung zur IndexedDB ermöglicht.
 var server;
+// Verbindungsherstellung zur IndexedDB
 db.open({
+	// Name der anzulegenden Datenbank
 	server: 'training-data-sync',
+	// Version der Datenbank, muss bei strukturellen Änderungen
+	// erhöht werden.
 	version: 1,
+	// Datenbank-Schema, ist flexibel
+	// Lediglich Key und AutoIncrement müssen angegeben werden.
 	schema: {
 		smallData: {
 			key: { keyPath: 'id', autoIncrement: true }
 		},
 		bigData: {
 			key: { keyPath: 'id', autoIncrement: true }
+		},
+		structureData1: {
+			key: { keyPath: 'id', autoIncrement: true }
+		},
+		structureData2: {
+			key: { keyPath: 'id', autoIncrement: true }
+		},
+		structureData3: {
+			key: { keyPath: 'id', autoIncrement: true }
 		}
 	}
 }).done(function (s) {
+	// Callback-Funktion erhält Objekt s
+	// Dieses wird in globaler Variable "server" gespeichert.
 	server = s;
 });
 
+/**
+ * Fügt ein Element zur IndexedDB hinzu.
+ * 
+ * Der Datentyp wird über die Variable "typ" übergeben,
+ * sodass erkennbar ist, in welcher Tabelle das neue Objekt
+ * gespeichert werden soll.
+ * Die Daten für das neue Objekt werden in der Variable
+ * "data" übergeben und auch so direkt in der Datenbank
+ * abgelegt.
+ * Die Variablen "i" und "n" sind nur bei mehrfachem Aufruf der
+ * Funktion von Bedeutung und erkennen, wann der letzte Aufruf erfolgt.
+ * Hierdurch ist es möglich die Zeit für bspw. 100 Einfügungen zu messen.
+ * 
+ * @param String typ
+ * @param Array data
+ * @param Integer i
+ * @param Integer n
+ */
 var dbAdd = function (typ, data, i, n) {
 	server.add(typ, data).done(function (items) {
 		// Neu erstelltes Objekt an den Server senden.
 		socket.emit('sync-up-single', {
-			typ: 'smallData',
+			typ: typ,
 			id: items[0].id,
 			data: {
 				content: items[0].content,
+				blob: items[0].blob,
 				deleted: items[0].deleted,
 				createdAt: items[0].createdAt,
 				updatedAt: items[0].updatedAt,
@@ -68,13 +107,27 @@ var dbAdd = function (typ, data, i, n) {
 		});
 		
 		console.log('Item (' + typ + ') stored with ID: ' + items[0].id + '.');
+		// Wenn letzter Aufruf erreicht und abgeschlossen,
+		// benötigte Zeit auf Konsole ausgeben und
+		// Ansicht über "dbFindAll" aktualisieren.
 		if (i == (n-1) && n != 1) {
 			console.timeEnd('dbAdd');
+			dbFindAll(typ);
 		}
-		dbFindAll(typ);
+		// Wenn nur 1 Einfügung stattgefunden hat,
+		// ebenfalls Ansicht über "dbFindAll" aktualisieren.
+		if (n == 1) {
+			dbFindAll(typ);
+		}
 	});
 };
 
+/**
+ * Löscht ein Element persistent aus der Datenbank
+ * 
+ * @param String typ
+ * @param Integer id
+ */
 var dbRemove = function (typ, id) {
 	server.remove(typ, id).done(function (key) {
 		console.log('Item (' + typ + ') with ID: ' + key + ' removed.');
@@ -82,13 +135,32 @@ var dbRemove = function (typ, id) {
 	});
 };
 
-var dbDelete = function (typ, id) {
+/**
+ * Setzt bei einem Element den Flag "deleted" auf 1.
+ * 
+ * @param String typ
+ * @param Integer id
+ * @param Integer i
+ * @param Integer n
+ */
+var dbDelete = function (typ, id, i, n) {
 	server.query(typ).filter('id', id).modify({deleted: 1}).execute().done(function (items) {
 		console.log('Item (' + typ + ') with ID: ' + items[0].id + ' deleted.');
-		dbFindAll(typ);
+		if (i == (n-1) && n != 1) {
+			console.timeEnd('dbFindAndDeleteAll');
+			dbFindAll(typ);
+		}
+		if (n == 1) {
+			dbFindAll(typ);
+		}
 	});
 };
 
+/**
+ * Löcht alle Element aus einem Objekt-Store.
+ * 
+ * @param String typ
+ */
 var dbClear = function (typ) {
 	server.clear(typ).done(function () {
 		console.log('Table ' + typ + ' cleared.');
@@ -96,6 +168,11 @@ var dbClear = function (typ) {
 	});
 };
 
+/**
+ * Ermittelt alle Elemente eines Objekt-Stores
+ * 
+ * @param String typ
+ */
 var dbFindAll = function (typ) {
 	console.time('dbFindAll');
 	server.query(typ).filter().execute().done(function (items) {
@@ -106,14 +183,33 @@ var dbFindAll = function (typ) {
 					<td>' + value.content + '</td> \
 					<td>' + value.deleted + '</td> \
 					<td>' + value.sync + '</td> \
-					<td>' + value.createdAt + '</td> \
-					<td>' + value.updatedAt + '</td> \
-					<td><a href="#" class="btn btn-danger" onclick="dbDelete(\'' + typ + '\', ' + value.id + ');">L&oumlschen</a></td> \
+					<td>' + value.createdAt.toLocaleString() + '</td> \
+					<td>' + value.updatedAt.toLocaleString() + '</td> \
+					<td><a href="#" class="btn btn-danger" onclick="dbDelete(\'' + typ + '\', ' + value.id + ', 1, 1);">Löschen</a></td> \
 					</tr> \
 			');
 		});
 		console.log('Anzahl der Eintraege: ' + items.length);
 		console.timeEnd('dbFindAll');
+	});
+};
+
+/**
+ * Setzt bei allen Elementen des Objekt-Stores den Flag
+ * "deleted" auf 1.
+ * 
+ * @param String typ
+ */
+var dbFindAndDeleteAll = function (typ) {
+	server.query(typ).filter().execute().done(function (items) {
+		console.time('dbFindAndDeleteAll');
+		var n = items.length;
+		var i = 0;
+		$.each(items, function (key, value) {
+			dbDelete(typ, value.id, i, n);
+			i++;
+		});
+		
 	});
 };
 /**
@@ -123,7 +219,7 @@ var dbFindAll = function (typ) {
 /**
  * BEGIN Verbindung zum Socket
  */
-// Array mit HTML-Code f�r den jeweiligen Verbindungs-Status
+// Array mit HTML-Code für den jeweiligen Verbindungs-Status
 var socketStatus = {
 	'online': '<span style="color: #5cb85c;"><i class="glyphicon glyphicon-ok-circle"></i> Online</span>',
 	'offline': '<span style="color: #d9534f;"><i class="glyphicon glyphicon-remove-circle"></i> Offline</span>',
@@ -136,8 +232,8 @@ var socketIP = settings.server.ip;
 // Port des Servers aus Einstellung
 var socketPort = settings.server.port;
 
-// Pr�fen der abgerufenen Einstellungen
-// IP und Port d�rfen nicht leer sein.
+// Prüfen der abgerufenen Einstellungen
+// IP und Port duerfen nicht leer sein.
 if (socketIP == undefined || socketPort == undefined) {
 	/**
 	 * @TODO: Fehlermeldung durch Fallback ersetzen.
@@ -150,18 +246,18 @@ if (socketIP == undefined || socketPort == undefined) {
 // socket-Objekt erhalten.
 var socket = io.connect('http://' + socketIP + ':' + socketPort);
 
-// Binding f�r den Verbindungsaufbau zum Socket
+// Binding fuer den Verbindungsaufbau zum Socket
 socket.on('connecting', function () {
 	$('#socketStatus').html(socketStatus.connect);
 	console.log('Verbindung zum Server wird aufgebaut...');
 });
 
-// Binding f�r die abgeschlossene Verbindungsherstellung
+// Binding fuer die abgeschlossene Verbindungsherstellung
 socket.on('connect', function () {
 	$('#socketStatus').html(socketStatus.online);
 	console.log('Verbindung zum Server hergestellt.');
 	
-	// Element ermitteln, die nicht synchronisiert sind.#
+	// Element ermitteln, die nicht synchronisiert sind.
 	// Dabei sind ALLE Datentypen zu durchlaufen.
 	server.query('smallData').filter('sync', false).execute().done(function (items) {
 		console.log(items);
@@ -172,7 +268,7 @@ socket.on('connect', function () {
 	});
 });
 
-// Binding f�r die Verbindungstrennung
+// Binding fuer die Verbindungstrennung
 socket.on('disconnect', function () {
 	$('#socketStatus').html(socketStatus.offline);
 	console.log('Verbindung zum Server getrennt.');
@@ -188,7 +284,7 @@ socket.on('sync-up-single-ok', function (data) {
 	});
 });
 socket.on('sync-down-single', function (data) {
-	App.__container__.lookup('controller:' + data.typ + 's').send('syncDownOne', data);
+	//App.__container__.lookup('controller:' + data.typ + 's').send('syncDownOne', data);
 });
 /**
  * END Verbindung zum Socket
@@ -197,24 +293,21 @@ socket.on('sync-down-single', function (data) {
 /**
  * BEGIN EventListener
  */
+
+// Listener fuer Menu-Punkte
 $('#menuSmallData').bind('click', function () {
 	$('#sidebar a').removeClass('active');
 	$(this).addClass('active');
+	addListener('smallData');
 	// Alle Elemente "smallData" finden.
 	dbFindAll('smallData');
 });
-$('#btnSmallDataAddOne').bind('click', function () {
-	smallDataAdd(1, 1);
-});
-
-$('#btnSmallDataAddMultiple').bind('click', function () {
-	console.time('dbAdd');
-	
-	var n = 100;
-	
-	for (var i = 0; i < n; i++) {
-		smallDataAdd(i, n);
-	}
+$('#menuBigData').bind('click', function () {
+	$('#sidebar a').removeClass('active');
+	$(this).addClass('active');
+	addListener('bigData');
+	// Alle Elemente "bigData" finden.
+	dbFindAll('bigData');
 });
 
 var smallDataAdd = function (i, n) {
@@ -222,6 +315,7 @@ var smallDataAdd = function (i, n) {
 	var data = {
 		serverId: null,
 		content: randomString(10),
+		blob: null,
 		deleted: 0,
 		sync: false,
 		createdAt: time,
@@ -230,17 +324,66 @@ var smallDataAdd = function (i, n) {
 	
 	dbAdd('smallData', data, i, n);
 };
+var bigDataAdd = function (i, n) {
+	var time = new Date();
+	var blob = randomBlob();
+	var size = (blob.length / 1024) / 1024;
+	size += ' MB';
+	
+	var data = {
+		serverId: null,
+		content: size,
+		blob: blob,
+		deleted: 0,
+		sync: false,
+		createdAt: time,
+		updatedAt: time
+	};
+	
+	dbAdd('bigData', data, i, n);
+}
 
-$('#btnSmallDataClear').bind('click', function () {
-	dbClear('smallData');
-});
 
-$('#menuBigData').bind('click', function () {
-	$('#sidebar a').removeClass('active');
-	$(this).addClass('active');
-	// Alle Elemente "bigData" finden.
-	dbFindAll('bigData');
-});
+var addListener = function (typ) {
+	$('#mainHeader').show();
+	$('#mainContent').show();
+	$('#emptyInfoText').html(typ);
+	
+	$('#btnClear').unbind();
+	$('#btnClear').bind('click', function () {
+		dbClear(typ);
+	});
+	
+	$('#btnDeleteAll').unbind();
+	$('#btnDeleteAll').bind('click', function () {
+		dbFindAndDeleteAll(typ);
+	});
+	
+	$('#btnAddOne').unbind();
+	$('#btnAddOne').bind('click', function () {
+		if (typ == 'smallData')	{
+			smallDataAdd(1, 1);
+		} else if (typ == 'bigData') {
+			bigDataAdd(1, 1);
+		}
+	});
+	
+	$('#btnAddMultiple').unbind();
+	$('#btnAddMultiple').bind('click', function () {
+		console.time('dbAdd');
+		
+		var n = 100;
+		
+		for (var i = 0; i < n; i++) {
+			if (typ == 'smallData')	{
+				smallDataAdd(i, n);
+			} else if (typ == 'bigData') {
+				console.log('bigData: ' + i);
+				bigDataAdd(i, n);
+			}
+		}
+	});
+};
 /**
  * END EventListener
  */
@@ -351,6 +494,17 @@ var randomString = function (length) {
 	
 	return text;
 };
+
+var randomBlob = function () {
+	var text = "";
+	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstvwxyz0123456789";
+	
+	for (var i = 0; i < (1024*1024*5); i++) {
+		text += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+	}
+	
+	return text; 
+}
 /**
  * END Helper Funktionen
  */
